075926f172136584481228f687aabb325830882b
fawziya zeyada <100339806+FawziyaMuhammed25@users.noreply.github.com>
2024-03-24 11:28:01 +0200

    Update HomeProduct.jsx

--- src/pages/HomeProduct.jsx
+++ src/pages/HomeProduct.jsx
@@ -3,12 +3,18 @@ import React from "react";
 export const HomeProduct = (props)=>{
    
     const {id , productName , price , productImage} = props.data;
-   return <div className="product">
+   return <div className=" col-lg-4 col-md-6 col-sm-12  ">
        
-        <img src={productImage}/>
-        <div className="description">
-              <p> <b>{productName}</b></p>
-              <p>{price} EGP</p>
-        </div>
+      
+       <div className="product" >
+            <div className="">
+                    <img src={productImage} className="w-100 "/>
+                </div>    
+                <div className="description">
+                    <p> <b>{productName}</b></p>
+                    <p>{price} EGP</p>
+                </div>
+       </div>
+      
     </div>
 }
