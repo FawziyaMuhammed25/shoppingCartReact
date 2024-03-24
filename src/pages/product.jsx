a393c58296070df79db4ed186b8311790ea595cf
fawziya zeyada <100339806+FawziyaMuhammed25@users.noreply.github.com>
2024-03-24 11:28:41 +0200

    Update product.jsx

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -4,13 +4,18 @@ export const Product = (props)=>{
     const {id , productName , price , productImage} = props.data;
     const {addToCart , cartItems} = useContext(ShopContext)
     const cartItemAmount = cartItems[id]
-   return <div className="product">
+   return <div className=" col-lg-4 col-md-6 col-sm-12  ">
        
-        <img src={productImage}/>
-        <div className="description">
-              <p> <b>{productName}</b></p>
-              <p>{price} EGP</p>
-        </div>
+      
+   <div className="product" >
+        <div className="">
+                <img src={productImage} className="w-100 "/>
+            </div>    
+            <div className="description">
+                <p> <b>{productName}</b></p>
+                <p>{price} EGP</p>
+            </div>
+   </div>
         <button className="addToCartBtn" onClick={()=>addToCart(id)}>Add To Cart {cartItemAmount >0 && <>({cartItemAmount})</>} </button>
     </div>
 }
